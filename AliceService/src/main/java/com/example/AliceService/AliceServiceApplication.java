f0372ea35e834db94454b64ffa798af765c607a2
Vinay Dighade <vinay.dighade95@gmail.com>
2022-07-16 00:07:54 +0530

    eureka removed
    removed eureka package since its trying to connect it

--- AliceService/src/main/java/com/example/AliceService/AliceServiceApplication.java
+++ AliceService/src/main/java/com/example/AliceService/AliceServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.AliceService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
