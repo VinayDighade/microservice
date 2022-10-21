57c6d406d207ad89f687cda09d0cf15b8f4c0dd7
Vinay <vinay.dighade95@gmail.com>
2022-10-21 20:32:54 +0530

    apigw changes

--- apigw/src/main/java/com/example/apigw/ApigwApplication.java
+++ apigw/src/main/java/com/example/apigw/ApigwApplication.java
@@ -1,19 +1,15 @@
 package com.example.apigw;
 
+import com.example.apigw.configuration.ApiGatewayConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-import com.example.apigw.configuration.ApiGatewayConfig;
-
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 @EnableFeignClients
 @Slf4j
 public class ApigwApplication {
