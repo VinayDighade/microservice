c24e73770bac08749d5eec788d824554766f2360
Vinay <vinay.dighade95@gmail.com>
2022-10-22 09:14:55 +0530

    Reset method added

--- AliceService/src/main/java/com/example/AliceService/controller/AliceController.java
+++ AliceService/src/main/java/com/example/AliceService/controller/AliceController.java
@@ -2,10 +2,7 @@ package com.example.AliceService.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.AliceService.AliceServiceConfiguration;
 
@@ -17,6 +14,7 @@ public class AliceController {
 	
 	private final AliceServiceConfiguration aliceServiceConfiguration;
 
+	public static Integer counter =  0;
 	@Autowired
 	public AliceController(AliceServiceConfiguration aliceServiceConfiguration) {
 		this.aliceServiceConfiguration = aliceServiceConfiguration;
@@ -27,5 +25,11 @@ public class AliceController {
 		log.info("[HelloAlice()] method is called");
 		return aliceServiceConfiguration.getMessage();
 
+	}
+
+	@GetMapping("/reset")
+	public void reset(){
+		counter = 0;
+		log.info("[Reset()] success");
 	}
 }
